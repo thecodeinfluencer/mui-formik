4ad32ba3405b3ccbf39a07898895242c5398c00a
Mark Aloo <mbaloo@safaricom.co.ke>
2024-06-10 16:10:27 +0300

    fix: import formula

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-export * from "./formik/FormikButton";
-export * from "./formik/FormikForm";
-export * from "./formik/FormikInput";
-export * from "./formik/FormikRadio";
-export * from "./formik/FormikSelect";
+export { default as FormikButton } from "./formik/FormikButton";
+export { default as FormikForm } from "./formik/FormikForm";
+export { default as FormikInput } from "./formik/FormikInput";
+export { default as FormikRadio } from "./formik/FormikRadio";
+export { default as FormikSelect } from "./formik/FormikSelect";
